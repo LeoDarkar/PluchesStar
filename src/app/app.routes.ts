8822a0ffa9069ba01203b4474696e58edb675c9f
LeoDarkar <leyundavid@gmail.com>
2024-12-30 23:13:53 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,23 +7,6 @@ export const routes: Routes = [
         loadComponent: () => import('./shared/components/layout/layout.component'),
         children: [
             {
-                path:'',
-                loadComponent: () => import('./shared/components/layout/layout.component'),
-            },
-            
-            {
-                path:'sections',
-                loadComponent: () => import('./shared/components/layout/layout.component'),
-            },
-            {
-                path:'profile',
-                loadComponent: () => import('./business/profile/profile.component')
-            },
-            {
-                path:'tables',
-                loadComponent: () => import('./business/tables/tables.component')
-            },
-            {
                 path: '',
                 redirectTo: 'sections',
                 pathMatch: 'full'
